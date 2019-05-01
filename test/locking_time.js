832a202d299f0dc3851abb61286bf843ce708ddf
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-05-01 16:56:07 -0300

    tests: remove unused imports

--- test/locking_time.js
+++ test/locking_time.js
@@ -1,14 +1,15 @@
 const { assertRevert } = require('@aragon/test-helpers/assertThrow')
-const getEvent = (receipt, event, arg) => { return receipt.logs.filter(l => l.event == event)[0].args[arg] }
+const getEvent = (receipt, event, arg) => { return receipt.logs.filter(l => l.event === event)[0].args[arg] }
 
 const Staking = artifacts.require('Staking');
+const StakingFactory = artifacts.require('StakingFactory');
 const StandardTokenMock = artifacts.require('StandardTokenMock');
 const TimeLockManagerMock = artifacts.require('TimeLockManagerMock');
 
 const fromBn = n => parseInt(n.valueOf(), 10)
 
-contract('Staking app, Time locking', accounts => {
-  let token, staking, manager, stakingAddress, tokenAddress, managerAddress, owner
+contract('Staking app, Time locking', ([owner]) => {
+  let token, staking, manager
 
   const TIME_UNIT_BLOCKS = 0
   const TIME_UNIT_SECONDS = 1
@@ -19,7 +20,7 @@ contract('Staking app, Time locking', accounts => {
   const EMPTY_STRING = ''
 
   const approveAndStake = async(amount = DEFAULT_AMOUNT) => {
-    await token.approve(stakingAddress, amount)
+    await token.approve(staking.address, amount)
     await staking.stake(amount, EMPTY_STRING)
   }
 
@@ -29,36 +30,27 @@ contract('Staking app, Time locking', accounts => {
       web3.fromDecimal(start).slice(2).padStart(64, "0") +
       web3.fromDecimal(end).slice(2).padStart(64, "0")
   }
-  const timeToData = (start, end) => {
-    return timeSpanToData(TIME_UNIT_SECONDS, start, end)
-  }
-  const blocksToData = (start, end) => {
-    return timeSpanToData(TIME_UNIT_BLOCKS, start, end)
-  }
+
+  const timeToData = (start, end) => timeSpanToData(TIME_UNIT_SECONDS, start, end)
+
+  const blocksToData = (start, end) => timeSpanToData(TIME_UNIT_BLOCKS, start, end)
 
   const approveStakeAndLock = async(data, lockAmount = DEFAULT_AMOUNT / 2, stakeAmount = DEFAULT_AMOUNT) => {
     await approveAndStake(stakeAmount)
-    const r = await staking.lock(lockAmount, managerAddress, data)
-    const lockId = getEvent(r, 'Locked', 'lockId')
-
-    return lockId
+    const r = await staking.lock(lockAmount, manager.address, data)
+    return getEvent(r, 'Locked', 'lockId')
   }
-
-  before(async () => {
-    owner = accounts[0]
-  })
 
   beforeEach(async () => {
     const initialAmount = 1000 * DEFAULT_AMOUNT
-    const tokenContract = await StandardTokenMock.new(owner, initialAmount)
-    token = tokenContract
-    tokenAddress = tokenContract.address
-    const stakingContract = await Staking.new(tokenAddress)
-    staking = stakingContract
-    stakingAddress = stakingContract.address
-    const managerContract = await TimeLockManagerMock.new()
-    manager = managerContract
-    managerAddress = managerContract.address
+    token = await StandardTokenMock.new(owner, initialAmount)
+
+    const factory = await StakingFactory.new()
+    const receipt = await factory.getOrCreateInstance(token.address)
+    const stakingAddress = getEvent(receipt, 'NewStaking', 'instance');
+    staking = Staking.at(stakingAddress)
+
+    manager = await TimeLockManagerMock.new()
   })
 
   it('locks using seconds', async () => {
@@ -71,7 +63,7 @@ contract('Staking app, Time locking', accounts => {
     const [ _amount, _unlockedAt, _manager, _data ] = await staking.getLock(owner, lockId)
     assert.equal(_amount, DEFAULT_AMOUNT / 2, "locked amount should match")
     assert.equal(_unlockedAt.toString(), (await manager.MAX_UINT64()).toString(), "unlock time should match")
-    assert.equal(_manager, managerAddress, "unlocker should match")
+    assert.equal(_manager, manager.address, "unlocker should match")
     assert.equal(_data, data, "lock data should match")
 
     // can not unlock
@@ -95,7 +87,7 @@ contract('Staking app, Time locking', accounts => {
     const [ _amount, _unlockedAt, _manager, _data ] = await staking.getLock(owner, lockId)
     assert.equal(_amount, DEFAULT_AMOUNT / 2, "locked amount should match")
     assert.equal(_unlockedAt.toString(), (await manager.MAX_UINT64()).toString(), "unlock time should match")
-    assert.equal(_manager, managerAddress, "unlocker should match")
+    assert.equal(_manager, manager.address, "unlocker should match")
     assert.equal(_data, data, "lock data should match")
 
     // can not unlock
@@ -137,7 +129,7 @@ contract('Staking app, Time locking', accounts => {
     const startTime2 = fromBn(await manager.getTimestampExt()) - 2
     const endTime2 = startTime2 + 1
     const data2 = timeToData(startTime2, endTime2)
-    await manager.setLockData(stakingAddress, owner, lockId, data2)
+    await manager.setLockData(staking.address, owner, lockId, data2)
 
 
     // check lock values
