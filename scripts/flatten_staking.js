aa3acbf9b7649727bca0a97749997a512c78fbe3
ßingen <bingen@aragon.one>
2020-06-09 22:42:30 +0200

    Update echidna tests (#38)

--- scripts/flatten_staking.js
+++
@@ -1,9 +0,0 @@
-const flatten = require('./helpers/flatten')
-
-module.exports = async () => {
-  const Token = artifacts.require('StandardTokenMock')
-  const token = await Token.new('0x0', 0)
-  const Staking = artifacts.require('Staking')
-  const staking = await Staking.new(token.address)
-  await flatten(staking)
-}
