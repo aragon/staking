bfe07bb5c05a2ca3b53267542945a3596721ab8f
ßingen <bingen@aragon.one>
2020-06-10 18:15:00 +0200

    Add tests for TimeLockManager lock callback

--- test/locking/lock_and_call.js
+++ test/locking/lock_and_call.js
@@ -6,17 +6,17 @@ const { deploy } = require('../helpers/deploy')(artifacts)
 const { DEFAULT_STAKE_AMOUNT, DEFAULT_LOCK_AMOUNT, EMPTY_DATA } = require('../helpers/constants')
 const { STAKING_ERRORS } = require('../helpers/errors')
 
-const BadLockManager = artifacts.require('BadLockManagerMock')
+const LockManagerMock = artifacts.require('LockManagerMock');
+const TimeLockManagerMock = artifacts.require('TimeLockManagerMock');
+const BadLockManagerMock = artifacts.require('BadLockManagerMock')
 
 const getDeepEventArgument = (receipt, contractAbi, eventName, argument, index=0) => {
   const logs = decodeEvents(receipt.receipt, contractAbi, eventName)
   return logs[index].args[argument]
 }
 
-const LockManagerMock = artifacts.require('LockManagerMock');
-
 contract('Staking app, Locking and calling', ([owner, user1, user2]) => {
-  let staking, token, lockManager
+  let staking, token
 
   const CALLBACK_DATA = sha3('receiveLock(uint256,uint256,bytes)').slice(0, 10)
 
@@ -24,16 +24,7 @@ contract('Staking app, Locking and calling', ([owner, user1, user2]) => {
     const deployment = await deploy(user1)
     token = deployment.token
     staking = deployment.staking
-    lockManager = deployment.lockManager
   })
-
-  const approveStakeAndLock = async (data) => {
-    await token.approve(staking.address, DEFAULT_STAKE_AMOUNT, { from: user1 })
-    await staking.stake(DEFAULT_STAKE_AMOUNT, EMPTY_DATA, { from: user1 })
-    const receipt = await staking.allowManagerAndLock(DEFAULT_LOCK_AMOUNT, lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
-
-    return receipt
-  }
 
   const checkCallbackLog = (receipt, data, lockAmount = DEFAULT_LOCK_AMOUNT) => {
     assert.equal(getDeepEventArgument(receipt, LockManagerMock.abi, 'LogLockCallback', 'amount'), lockAmount, 'Amount in callback should match')
@@ -41,66 +32,90 @@ contract('Staking app, Locking and calling', ([owner, user1, user2]) => {
     assert.equal(getDeepEventArgument(receipt, LockManagerMock.abi, 'LogLockCallback', 'data'), data, 'Data in callback should match')
   }
 
-  describe('allows lock manager and locks', () => {
-    it('and calls lock manager, with just the signature', async () => {
-      const data = CALLBACK_DATA
-      const receipt = await approveStakeAndLock(data)
-      checkCallbackLog(receipt, data)
+  const allowsLockManager = (LockManager) => {
+    let lockManager
+
+    const approveStakeAndLock = async (data) => {
+      await token.approve(staking.address, DEFAULT_STAKE_AMOUNT, { from: user1 })
+      await staking.stake(DEFAULT_STAKE_AMOUNT, EMPTY_DATA, { from: user1 })
+      const receipt = await staking.allowManagerAndLock(DEFAULT_LOCK_AMOUNT, lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
+
+      return receipt
+    }
+
+    beforeEach('deploy lock manager', async () => {
+      lockManager = await LockManager.new()
     })
 
-    it('and calls lock manager, with added data', async () => {
-      const data = CALLBACK_DATA + '0'.repeat(63) + '1'
-      const receipt = await approveStakeAndLock(data)
-      checkCallbackLog(receipt, data)
+    describe('allows lock manager and locks', () => {
+      it('and calls lock manager, with just the signature', async () => {
+        const data = CALLBACK_DATA
+        const receipt = await approveStakeAndLock(data)
+        checkCallbackLog(receipt, data)
+      })
+
+      it('and calls lock manager, with added data', async () => {
+        const data = CALLBACK_DATA + '0'.repeat(63) + '1'
+        const receipt = await approveStakeAndLock(data)
+        checkCallbackLog(receipt, data)
+      })
+
+      it('but doesn’t call lock manager without proper data', async () => {
+        // some random data
+        const data = '0x1234'
+        const receipt = await approveStakeAndLock(data)
+
+        assert.equal(decodeEvents(receipt.receipt, LockManager.abi, 'LogLockCallback').length, 0, 'There should be no logs')
+      })
+
+      it('but doesn’t call lock manager with empty data', async () => {
+        const receipt = await approveStakeAndLock(EMPTY_DATA)
+
+        assert.equal(decodeEvents(receipt.receipt, LockManager.abi, 'LogLockCallback').length, 0, 'There should be no logs')
+      })
     })
 
-    it('but doesn’t call lock manager without proper data', async () => {
-      // some random data
-      const data = '0x1234'
-      const receipt = await approveStakeAndLock(data)
+    describe('allows lock manager without locking', () => {
+      it('and calls lock manager, with just the signature', async () => {
+        const data = CALLBACK_DATA
+        const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
+        checkCallbackLog(receipt, data, 0)
+      })
 
-      assert.equal(decodeEvents(receipt.receipt, LockManagerMock.abi, 'LogLockCallback').length, 0, 'There should be no logs')
+      it('and calls lock manager, with added data', async () => {
+        const data = CALLBACK_DATA + '0'.repeat(63) + '1'
+        const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
+        checkCallbackLog(receipt, data, 0)
+      })
+
+      it('but doesn’t call lock manager without proper data', async () => {
+        // some random data
+        const data = '0x1234'
+        const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
+
+        assert.equal(decodeEvents(receipt.receipt, LockManager.abi, 'LogLockCallback').length, 0, 'There should be no logs')
+      })
+
+      it('but doesn’t call lock manager with empty data', async () => {
+        const data = EMPTY_DATA
+        const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
+
+        assert.equal(decodeEvents(receipt.receipt, LockManager.abi, 'LogLockCallback').length, 0, 'There should be no logs')
+      })
     })
+  }
 
-    it('but doesn’t call lock manager with empty data', async () => {
-      const receipt = await approveStakeAndLock(EMPTY_DATA)
-
-      assert.equal(decodeEvents(receipt.receipt, LockManagerMock.abi, 'LogLockCallback').length, 0, 'There should be no logs')
-    })
+  describe('regular lock manager', () => {
+    allowsLockManager(LockManagerMock)
   })
 
-  describe('allows lock manager without locking', () => {
-    it('and calls lock manager, with just the signature', async () => {
-      const data = CALLBACK_DATA
-      const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
-      checkCallbackLog(receipt, data, 0)
-    })
-
-    it('and calls lock manager, with added data', async () => {
-      const data = CALLBACK_DATA + '0'.repeat(63) + '1'
-      const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
-      checkCallbackLog(receipt, data, 0)
-    })
-
-    it('but doesn’t call lock manager without proper data', async () => {
-      // some random data
-      const data = '0x1234'
-      const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
-
-      assert.equal(decodeEvents(receipt.receipt, LockManagerMock.abi, 'LogLockCallback').length, 0, 'There should be no logs')
-    })
-
-    it('but doesn’t call lock manager with empty data', async () => {
-      const data = EMPTY_DATA
-      const receipt = await staking.allowManager(lockManager.address, DEFAULT_STAKE_AMOUNT, data, { from: user1 })
-
-      assert.equal(decodeEvents(receipt.receipt, LockManagerMock.abi, 'LogLockCallback').length, 0, 'There should be no logs')
-    })
+  describe('time lock manager', () => {
+    allowsLockManager(TimeLockManagerMock)
   })
 
   describe('Bad lock manager', () => {
     it('fails calling if lock manager doesn’t return true', async () => {
-      const badLockManager = await BadLockManager.new()
+      const badLockManager = await BadLockManagerMock.new()
 
       await token.approve(staking.address, DEFAULT_STAKE_AMOUNT, { from: user1 })
       await staking.stake(DEFAULT_STAKE_AMOUNT, EMPTY_DATA, { from: user1 })
