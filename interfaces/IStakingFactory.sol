cbb2a89ceb557ccdf15dc5659a882620f78bd962
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-20 13:26:35 -0300

    Contracts: Fix exportes staking factory interface

--- interfaces/IStakingFactory.sol
+++ interfaces/IStakingFactory.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4 <=0.7;
 import "./IStaking.sol";
 
 
-contract StakingFactory {
+interface IStakingFactory {
     function existsInstance(/* ERC20 */ address token) external view returns (bool);
     function getInstance(/* ERC20 */ address token) external view returns (IStaking);
     function getOrCreateInstance(/* ERC20 */ address token) external returns (IStaking);
