59ff6a339858b0b055d282322b180bc3f68b7a46
ßingen <bingen@aragon.one>
2020-09-08 19:43:50 +0200

    Minor changes to IStakingLocking

--- packages/protocol/contracts/locking/IStakingLocking.sol
+++ packages/protocol/contracts/locking/IStakingLocking.sol
@@ -33,8 +33,7 @@ interface IStakingLocking {
 
     function getLock(address _account, address _lockManager) external view returns (uint256 _amount, uint256 _allowance);
     function unlockedBalanceOf(address _account) external view returns (uint256);
-    // Note: seems weird to use both 'user' and 'account' as names, if they represent the same concept
-    function lockedBalanceOf(address _user) external view returns (uint256);
-    function getBalancesOf(address _user) external view returns (uint256 staked, uint256 locked);
+    function lockedBalanceOf(address _account) external view returns (uint256);
+    function getBalancesOf(address _account) external view returns (uint256 staked, uint256 locked);
     function canUnlock(address _sender, address _account, address _lockManager, uint256 _amount) external view returns (bool);
 }
