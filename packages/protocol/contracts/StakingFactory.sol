e11478c2400373afff82e54af87a9250af9a1904
ßingen <bingen@aragon.one>
2020-09-08 19:35:24 +0200

    Style changes to StakingFactory

--- packages/protocol/contracts/StakingFactory.sol
+++ packages/protocol/contracts/StakingFactory.sol
@@ -8,26 +8,24 @@ import "./Staking.sol";
 contract StakingFactory {
     mapping (address => address) internal instances;
 
-    // Note: we might as well index the token
-    event NewStaking(address indexed instance, address token);
+    event NewStaking(address indexed instance, address indexed token);
 
-    // Note: in terms of code style, should we align all the params to have the _ prefix?
-    function existsInstance(ERC20 token) external view returns (bool) {
-        return address(getInstance(token)) != address(0);
+    function existsInstance(ERC20 _token) external view returns (bool) {
+        return address(getInstance(_token)) != address(0);
     }
 
-    function getOrCreateInstance(ERC20 token) external returns (Staking) {
-        Staking instance = getInstance(token);
-        return address(instance) != address(0) ? instance : _createInstance(token);
+    function getOrCreateInstance(ERC20 _token) external returns (Staking) {
+        Staking instance = getInstance(_token);
+        return address(instance) != address(0) ? instance : _createInstance(_token);
     }
 
-    function getInstance(ERC20 token) public view returns (Staking) {
-        return Staking(instances[address(token)]);
+    function getInstance(ERC20 _token) public view returns (Staking) {
+        return Staking(instances[address(_token)]);
     }
 
-    function _createInstance(ERC20 token) internal returns (Staking) {
-        Staking instance = new Staking(token);
-        address tokenAddress = address(token);
+    function _createInstance(ERC20 _token) internal returns (Staking) {
+        Staking instance = new Staking(_token);
+        address tokenAddress = address(_token);
         address instanceAddress = address(instance);
         instances[tokenAddress] = instanceAddress;
         emit NewStaking(instanceAddress, tokenAddress);
