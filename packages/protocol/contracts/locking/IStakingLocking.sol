e50ea296e4a9d00279e7af127e6b26297e460b3a
ßingen <bingen@aragon.one>
2020-09-09 21:58:21 +0200

    Index some IStakingLocking events

--- packages/protocol/contracts/locking/IStakingLocking.sol
+++ packages/protocol/contracts/locking/IStakingLocking.sol
@@ -1,20 +1,14 @@
 pragma solidity ^0.5.17;
 
 
-// Note: seems weird that this is in the locking/ dir; perhaps we should move it up to be the same dir as Staking?
-// Note: perhaps we can find a better name... like StakingWithLockManagement?
 interface IStakingLocking {
     event NewLockManager(address indexed account, address indexed lockManager, bytes data);
     event Unlocked(address indexed account, address indexed lockManager, uint256 amount);
-    // Note: the increase param for these two does not seem that useful
     event LockAmountChanged(address indexed account, address indexed lockManager, uint256 amount, bool increase);
     event LockAllowanceChanged(address indexed account, address indexed lockManager, uint256 allowance, bool increase);
-    // Note: should the lock manager param be indexed?
-    event LockManagerRemoved(address indexed account, address lockManager);
-    // Note: should the new lock manager param be indexed?
-    event LockManagerTransferred(address indexed account, address indexed oldLockManager, address newLockManager);
-    // Note: should the to param be indexed?
-    event StakeTransferred(address indexed from, address to, uint256 amount);
+    event LockManagerRemoved(address indexed account, address indexed lockManager);
+    event LockManagerTransferred(address indexed account, address indexed oldLockManager, address indexed newLockManager);
+    event StakeTransferred(address indexed from, address indexed to, uint256 amount);
 
     function allowManager(address _lockManager, uint256 _allowance, bytes calldata _data) external;
     function unlockAndRemoveManager(address _account, address _lockManager) external;
