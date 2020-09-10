92a4a70e0ad3f1fc7676eb71945abcbef21e1c44
ßingen <bingen@aragon.one>
2020-09-10 09:55:54 +0200

    Add missing headers for IERC900

--- packages/protocol/contracts/standards/IERC900.sol
+++ packages/protocol/contracts/standards/IERC900.sol
@@ -53,7 +53,25 @@ interface IERC900 {
      */
     function supportsHistory() external pure returns (bool);
 
-    function lastStakedFor(address addr) external view returns (uint256);
-    function totalStakedForAt(address addr, uint256 blockNumber) external view returns (uint256);
-    function totalStakedAt(uint256 blockNumber) external view returns (uint256);
+    /**
+     * @dev Get last time a user modified its staked balance
+     * @param _user Account requesting for
+     * @return Last block number when account's balance was modified
+     */
+    function lastStakedFor(address _user) external view returns (uint256);
+
+    /**
+     * @dev Get the total amount of tokens staked by a user at a given block number
+     * @param _user Account requesting for
+     * @param _blockNumber Block number at which we are requesting
+     * @return The amount of tokens staked by the account at the given block number
+     */
+    function totalStakedForAt(address _user, uint256 _blockNumber) external view returns (uint256);
+
+    /**
+     * @dev Get the total amount of tokens staked by all users at a given block number
+     * @param _blockNumber Block number at which we are requesting
+     * @return The amount of tokens staked at the given block number
+     */
+    function totalStakedAt(uint256 _blockNumber) external view returns (uint256);
 }
