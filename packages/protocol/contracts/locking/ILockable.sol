d9faa09f15d19bea613e04a2f7e82166ac5ee4d3
Brett Sun <qisheng.brett.sun@gmail.com>
2020-09-10 11:18:16 +0200

    Interfaces: align on 'user' rather than 'account'

--- packages/protocol/contracts/locking/ILockable.sol
+++ packages/protocol/contracts/locking/ILockable.sol
@@ -2,25 +2,25 @@ pragma solidity ^0.5.17;
 
 
 interface ILockable {
-    event NewLockManager(address indexed account, address indexed lockManager, bytes data);
-    event LockAmountChanged(address indexed account, address indexed lockManager, uint256 amount);
-    event LockAllowanceChanged(address indexed account, address indexed lockManager, uint256 allowance);
-    event LockManagerRemoved(address indexed account, address indexed lockManager);
-    event LockManagerTransferred(address indexed account, address indexed oldLockManager, address indexed newLockManager);
+    event NewLockManager(address indexed user, address indexed lockManager, bytes data);
+    event LockAmountChanged(address indexed user, address indexed lockManager, uint256 amount);
+    event LockAllowanceChanged(address indexed user, address indexed lockManager, uint256 allowance);
+    event LockManagerRemoved(address indexed user, address indexed lockManager);
+    event LockManagerTransferred(address indexed user, address indexed oldLockManager, address indexed newLockManager);
 
     function allowManager(address _lockManager, uint256 _allowance, bytes calldata _data) external;
-    function unlockAndRemoveManager(address _account, address _lockManager) external;
+    function unlockAndRemoveManager(address _user, address _lockManager) external;
     function increaseLockAllowance(address _lockManager, uint256 _allowance) external;
-    function decreaseLockAllowance(address _account, address _lockManager, uint256 _allowance) external;
+    function decreaseLockAllowance(address _user, address _lockManager, uint256 _allowance) external;
 
-    function lock(address _account, uint256 _amount) external;
-    function unlock(address _account, address _lockManager, uint256 _amount) external;
-    function slash(address _account, address _to, uint256 _amount) external;
-    function slashAndUnstake(address _account, address _to, uint256 _amount) external;
+    function lock(address _user, uint256 _amount) external;
+    function unlock(address _user, address _lockManager, uint256 _amount) external;
+    function slash(address _user, address _to, uint256 _amount) external;
+    function slashAndUnstake(address _user, address _to, uint256 _amount) external;
 
-    function getLock(address _account, address _lockManager) external view returns (uint256 _amount, uint256 _allowance);
-    function unlockedBalanceOf(address _account) external view returns (uint256);
-    function lockedBalanceOf(address _account) external view returns (uint256);
-    function getBalancesOf(address _account) external view returns (uint256 staked, uint256 locked);
-    function canUnlock(address _sender, address _account, address _lockManager, uint256 _amount) external view returns (bool);
+    function getLock(address _user, address _lockManager) external view returns (uint256 _amount, uint256 _allowance);
+    function unlockedBalanceOf(address _user) external view returns (uint256);
+    function lockedBalanceOf(address _user) external view returns (uint256);
+    function getBalancesOf(address _user) external view returns (uint256 staked, uint256 locked);
+    function canUnlock(address _sender, address _user, address _lockManager, uint256 _amount) external view returns (bool);
 }
